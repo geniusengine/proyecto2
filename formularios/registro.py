89a7430e18e0ccae891199bcb9980f21e832fa3a
geniusengine <contacto.geniusengine@gmail.com>
2023-10-28 20:09:34 -0300

    kljlkjjk
    kljlkj

--- formularios/registro.py
+++ formularios/registro.py
@@ -10,10 +10,10 @@ Auteur: danie(mitchel.dmch@gmail.com)
 registro.py(Ɔ) 2023
 Description : Saisissez la description puis « Tab »
 Créé le :  samedi 26 août 2023 à 18:37:13 
-Dernière modification : mercredi 11 octobre 2023 à 20:39:26
+Dernière modification : samedi 28 octobre 2023 à 19:59:11
 """
 import sys
-import mysql.connector
+import pymssql
 from passlib.hash import bcrypt
 from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget, QMessageBox
 
@@ -34,9 +34,13 @@ class RegisterApp(QMainWindow):
         self.setCentralWidget(self.central_widget)
 
         self.layout = QVBoxLayout()
-
+        
+        self.rut_label = QLabel("Rut:")
+        self.rut_input = QLineEdit()
         self.name_label = QLabel("Nombre:")
         self.name_input = QLineEdit()
+        self.apellido_label = QLabel("Apellido:")
+        self.apellido_input = QLineEdit()
         self.email_label = QLabel("Correo Electrónico:")
         self.email_input = QLineEdit()
         self.username_label = QLabel("Usuario:")
@@ -47,8 +51,12 @@ class RegisterApp(QMainWindow):
         self.register_button = QPushButton("Registrar")
         self.register_button.clicked.connect(self.register_user)
 
+        self.layout.addWidget(self.rut_label)
+        self.layout.addWidget(self.rut_input)
         self.layout.addWidget(self.name_label)
         self.layout.addWidget(self.name_input)
+        self.layout.addWidget(self.apellido_label)
+        self.layout.addWidget(self.apellido_input)
         self.layout.addWidget(self.email_label)
         self.layout.addWidget(self.email_input)
         self.layout.addWidget(self.username_label)
@@ -60,20 +68,22 @@ class RegisterApp(QMainWindow):
         self.central_widget.setLayout(self.layout)
 
     def init_db(self):
-        self.db = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="",
-            database="mi_causa"
-        )
+        self.db = pymssql.connect(
+                server='vps-3697915-x.dattaweb.com',
+                user='daniel',
+                password='LOLxdsas--',
+                database='micau5a'
+            )
 
-        if not self.db.is_connected():
+        if not self.db:
             print("Error al conectar a la base de datos MySQL.")
             sys.exit(1)
 
         self.cursor = self.db.cursor()
     def register_user(self):
+        rut = self.rut_input.text()
         name = self.name_input.text()
+        apellido = self.apellido_input.text()
         email = self.email_input.text()
         username = self.username_input.text()
         password = self.password_input.text()
@@ -87,14 +97,16 @@ class RegisterApp(QMainWindow):
         hashed_password = bcrypt.hash(password)
 
     # Insertar el nuevo usuario en la base de datos MySQL
-        sql = "INSERT INTO usuarios (name, email, username, password) VALUES (%s, %s, %s, %s)"
-        values = (name, email, username, hashed_password)
+        sql = "INSERT INTO usuarios (rut,nombreusuario,apellidousuario,email, username, password) VALUES (%s,%s,%s, %s, %s, %s)"
+        values = (rut,name,apellido, email, username, hashed_password)
 
         try:
             self.cursor.execute(sql, values)
             self.db.commit()
             QMessageBox.information(self, "Registro Exitoso", "Usuario registrado correctamente.")
+            self.rut_input.clear()
             self.name_input.clear()
+            self.apellido_input.clear()
             self.email_input.clear()
             self.username_input.clear()
             self.password_input.clear()
