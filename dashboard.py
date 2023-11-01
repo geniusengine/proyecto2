640cc93ff48032742e2f769c890e435dce96ce17
Bastian <bastiansotopizarro@gmail.com>
2023-11-01 16:28:33 -0300

    hace la conexion entre el boton y la ventana

--- dashboard.py
+++ dashboard.py
@@ -3,6 +3,7 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout,
 from PyQt6.QtGui import QColor
 import pymssql
 from verCausa import VerCausaApp
+from buscado import BuscadorDatosCausaApp
 
 class DashboardApp(QMainWindow):
     def __init__(self):
@@ -40,7 +41,8 @@ class DashboardApp(QMainWindow):
     def buscar_clicked(self):
         # Manejar la lógica cuando se hace clic en el botón de buscar
         #llama a la ventana de busqueda de causas
-        
+        self.buscar_app = BuscadorDatosCausaApp()
+        self.buscar_app.show()
 
         pass
 
