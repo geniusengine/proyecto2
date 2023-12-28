d9d07104b62f6e9d0f0b8b6b5774aa4e102264aa
Knarap <104379279+Knarap@users.noreply.github.com>
2023-12-28 12:12:28 -0300

    Merge branch 'main' of https://github.com/geniusengine/proyecto2

--- funcionalidades/buscado.py
+++ funcionalidades/buscado.py
@@ -73,8 +73,8 @@ class BuscadorDatosCausaApp(QMainWindow):
             cursor = connection.cursor()
 
             query = """
-            SELECT numjui, nombtribunal
-            FROM demanda
+            SELECT fechaNotificacion, numjui, nombTribunal, nombdemandante, apellidemandante, nombdemandado, apellidemandado, nombmandante, apellimandante, repre, domicilio, comuna, soli, encargo, arancel, estadoNoti, estadoCausa
+            FROM notificacion
             WHERE numjui = %s OR nombtribunal = %s
             """
             cursor.execute(query, (numjui, numjui))
@@ -127,8 +127,14 @@ class BuscadorDatosCausaApp(QMainWindow):
             selected_data = []
             for result_item in selected_results:
                 numjui = result_item.text().split(',')[0].split(':')[-1].strip()
-                query = "SELECT fechaNotificacion, numjui, nombTribunal, nombdemandante, apellidemandante, nombdemandado, apellidemandado, nombmandante, apellimandante, repre, domicilio, comuna, soli, encargo, arancel, estadoNoti, estadoCausa FROM notificacion"
-                cursor.execute(query, (numjui,))
+                nombtribunal = result_item.text().split(',')[1].split(':')[-1].strip()
+                
+                query = """
+            SELECT fechaNotificacion, numjui, nombTribunal, nombdemandante, apellidemandante, nombdemandado, apellidemandado, nombmandante, apellimandante, repre, domicilio, comuna, soli, encargo, arancel, estadoNoti, estadoCausa
+            FROM notificacion
+            WHERE numjui = %s OR nombtribunal = %s
+            """
+                cursor.execute(query, (numjui, nombtribunal))
                 data = cursor.fetchall()
                 selected_data.extend(data)
 
